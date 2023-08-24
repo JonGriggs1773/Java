104ba0accc1c8c47f4284bb259256cadd8c631d6
Jon Griggs <bigmac_beats1773@outlook.com>
2023-08-24 14:45:15 -0500

    first commit

---
+++ fundamental/First_Java_Program/Home.java
@@ -0,0 +1,8 @@
+
+public class Home {
+    public static void main(String[] args) {
+        System.out.println("My name is Jonathen Griggs");
+        System.out.println("I am 23 Years old");
+        System.out.println("My hometown is Five Points, TN");
+    }
+}
